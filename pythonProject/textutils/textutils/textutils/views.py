a8a33270e9f8637dafee7ec74760ab4e7d60cf91
Debmalya Biswas <68019329+AnTIdoTe003@users.noreply.github.com>
2022-06-16 12:27:32 +0530

    Add files via upload

---
+++ pythonProject/textutils/textutils/textutils/views.py
@@ -0,0 +1,34 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import random
+
+
+def index(request):
+    # return HttpResponse("Home where we will take the user input ")
+    return render(request, 'index.html')
+
+
+def output(request):
+    user_input = (request.GET.get('num', 'default'))
+    DIGITS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    LOCASE_CHARACTERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+                         'i', 'j', 'k', 'm', 'n', 'o', 'p', 'q',
+                         'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+                         'z']
+
+    UPCASE_CHARACTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
+                         'I', 'J', 'K', 'M', 'N', 'O', 'p', 'Q',
+                         'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+                         'Z']
+
+    SYMBOLS = ['@', '#', '$', '%', '=', ':', '?', '.', '/', '|', '~', '>',
+               '*', '(', ')', '<']
+
+    COMBINED_LIST = DIGITS + UPCASE_CHARACTERS + LOCASE_CHARACTERS + SYMBOLS
+
+    password = ""
+    for i in range(int(user_input)):
+        password = password + random.choice(COMBINED_LIST)
+    para = {'result': password}
+
+    return render(request, 'output.html', para)
